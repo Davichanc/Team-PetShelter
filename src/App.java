27ea9e1929901ba4c7d49cc41e66adfe7b39ab1b
Davichanc <133520538+Davichanc@users.noreply.github.com>
2023-07-23 16:22:39 -0400

    pet abstrat las t commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, This is the Pet Shelter:");
+        boolean option1 = true;
+        while (option1) {
+            if(opti)
+            System.out.println("What you want to do?");
+            System.out.println("0. Exit n\1. Create a pet ");
+        }
     }
 }
